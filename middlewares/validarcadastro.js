4bde2c2e6c8b7b1cc164281e77d1ebf82f6578a1
Diego Magalhaes <brandaodiegof@gmail.com>
2021-04-20 11:59:31 -0300

    interação back com front usando ejs

--- middlewares/validarcadastro.js
+++ middlewares/validarcadastro.js
@@ -5,17 +5,14 @@ module.exports = async (request, response, next) => {
     let { email, senha, nome } = request.body;
 
     let user = await Usuario.findAll({ where: { email } })
-    if (user.length) {
-        response.status(400).json({ erro: "Email já cadastrado" })
-        return
+    if (!nome || !email || !senha) {
+        return response.status(400).json({ erro: "Informe todos os dados" })
     } else {
-        if (senha.length < 6 || senha.length > 12) {
-            response.status(400).json({ erro: "Senha não deve ser menor que 6 caracteres ou maior que 12." })
-            return
+        if (user.length) {
+            return response.status(400).json({ erro: "Email já cadastrado" })
         } else {
-            if (!nome || !email || !senha) {
-                response.status(400).json({ erro: "Informe todos os dados" })
-                return
+            if (senha.length < 6 || senha.length > 12) {
+                return response.status(400).json({ erro: "Senha não deve ser menor que 6 caracteres ou maior que 12." })
             } else {
                 next();
             }
